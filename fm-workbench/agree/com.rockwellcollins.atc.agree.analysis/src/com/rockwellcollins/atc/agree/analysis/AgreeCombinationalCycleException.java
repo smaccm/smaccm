fe6281887657b90ba456459e069736bb216eef70
Backes <john.backes@gmail.com>
2013-11-26 10:20:12 -0600

    adding untracked files

---
+++ fm-workbench/agree/com.rockwellcollins.atc.agree.analysis/src/com/rockwellcollins/atc/agree/analysis/AgreeCombinationalCycleException.java
@@ -0,0 +1,12 @@
+package com.rockwellcollins.atc.agree.analysis;
+
+public class AgreeCombinationalCycleException extends AgreeException {
+
+    private static final long serialVersionUID = 7812187127885265161L;
+
+    public AgreeCombinationalCycleException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+}
