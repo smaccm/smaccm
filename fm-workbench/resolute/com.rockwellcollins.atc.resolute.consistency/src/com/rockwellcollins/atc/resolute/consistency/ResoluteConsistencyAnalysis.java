22bde88693c216d9b773c5667c4e68c15548e57b
Julien Delange <julien@gunnm.org>
2013-12-18 15:55:38 -0500

    * Start to implement the consistency verification methods

--- fm-workbench/resolute/com.rockwellcollins.atc.resolute.consistency/src/com/rockwellcollins/atc/resolute/consistency/ResoluteConsistencyAnalysis.java
+++ fm-workbench/resolute/com.rockwellcollins.atc.resolute.consistency/src/com/rockwellcollins/atc/resolute/consistency/ResoluteConsistencyAnalysis.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
 
+import org.osate.aadl2.NamedElement;
+import org.osate.aadl2.util.OsateDebug;
+
 import com.rockwellcollins.atc.resolute.analysis.ResoluteEvaluator;
 import com.rockwellcollins.atc.resolute.analysis.ResoluteExternalAnalysis;
 import com.rockwellcollins.atc.resolute.analysis.values.BoolValue;
@@ -20,14 +23,35 @@ public class ResoluteConsistencyAnalysis implements ResoluteExternalAnalysis {
     	ResoluteValue arg1 = args.get(1);
     	ResoluteValue arg2 = args.get(2);
     	ResoluteValue arg3 = args.get(3);
+    	ResoluteValue arg4 = args.get(4);
     	
     	assert (arg0.isString());
-    	assert (arg1.isString());
+    	assert (arg1.isNamedElement());
     	assert (arg2.isString());
     	assert (arg3.isString());
-    			
-
-        return new BoolValue(true);
+    	assert (arg4.isString());
+    	
+    	NamedElement component = arg1.getNamedElement();
+    	String modelType = arg2.getString();
+    	String checkType = arg3.getString();
+    	String filename = arg4.getString();
+    	
+    	OsateDebug.osateDebug("[ResoluteConsistencyAnalysis] component="+component.getName()+";modelType="+modelType+";checkType="+checkType+"filename="+filename);
+        
+    	if (modelType.equalsIgnoreCase("geometry_stp"))
+    	{
+    		/*
+    		 * Geometry-specific semantics checks
+    		 */
+    	}
+    	
+    	if (modelType.equalsIgnoreCase("semantics checks"))
+    	{
+    		/*
+    		 * Geometry-specific semantics checks
+    		 */
+    	}
+    	return new BoolValue(true);
     }
 
 }
---
+++ fm-workbench/resolute/com.rockwellcollins.atc.resolute.consistency/src/com/rockwellcollins/atc/resolute/consistency/Utils.java
@@ -0,0 +1,6 @@
+package com.rockwellcollins.atc.resolute.consistency;
+
+public class Utils {
+	public enum ModelType 		{GEOMETRY_STEP,SIMULINK}
+	public enum ConsistencyType {ANY,ALL,EXIST}
+}
