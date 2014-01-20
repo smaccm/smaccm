fd724f820d733a6cae1ed62b00eb7546e59cd02d
Backes <john.backes@gmail.com>
2014-01-20 11:48:52 -0600

    adding dot printing to resolute

--- fm-workbench/resolute/com.rockwellcollins.atc.resolute.analysis/src/com/rockwellcollins/atc/resolute/analysis/views/AssuranceCaseView.java
+++ fm-workbench/resolute/com.rockwellcollins.atc.resolute.analysis/src/com/rockwellcollins/atc/resolute/analysis/views/AssuranceCaseView.java
@@ -20,12 +20,22 @@ import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.xtext.ui.editor.GlobalURIEditorOpener;
 
 import com.google.inject.Inject;
 import com.rockwellcollins.atc.resolute.analysis.results.ClaimResult;
 import com.rockwellcollins.atc.resolute.analysis.results.FailResult;
+import com.rockwellcollins.atc.resolute.analysis.results.ResoluteDOTUtils;
 import com.rockwellcollins.atc.resolute.analysis.results.ResoluteResult;
 import com.rockwellcollins.atc.resolute.resolute.ProveStatement;
 
@@ -58,6 +68,7 @@ public class AssuranceCaseView extends ViewPart {
                         manager.add(createHyperlinkAction("Open Failure Location", location));
                     } else if (location instanceof ProveStatement) {
                         manager.add(createHyperlinkAction("Open Prove Statement", location));
+                        manager.add(createWriteConsoleAction("Show DOT Text In Console", "dotConsole", claim));
                     } else {
                         manager.add(createHyperlinkAction("Open Claim Definition", location));
                     }
@@ -91,6 +102,48 @@ public class AssuranceCaseView extends ViewPart {
         };
     }
 
+    private Action createWriteConsoleAction(String actionName, final String consoleName,
+            final ClaimResult claim) {
+        return new Action(actionName) {
+            @Override
+            public void run() {
+                MessageConsole console = findConsole(consoleName);
+                showConsole(console);
+                console.clearConsole();
+                console.newMessageStream().println(ResoluteDOTUtils.claimToDOTText(claim));
+            }
+        };
+    }
+    
+    
+    
+    
+    private static MessageConsole findConsole(String name) {
+        ConsolePlugin plugin = ConsolePlugin.getDefault();
+        IConsoleManager conMan = plugin.getConsoleManager();
+        IConsole[] existing = conMan.getConsoles();
+        for (int i = 0; i < existing.length; i++) {
+            if (name.equals(existing[i].getName())) {
+                return (MessageConsole) existing[i];
+            }
+        }
+        // no console found, so create a new one
+        MessageConsole myConsole = new MessageConsole(name, null);
+        conMan.addConsoles(new IConsole[] { myConsole });
+        return myConsole;
+    }
+
+    
+    private void showConsole(IConsole console) {
+        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        try {
+            IConsoleView view = (IConsoleView) page.showView(IConsoleConstants.ID_CONSOLE_VIEW);
+            view.display(console);
+            view.setScrollLock(true);
+        } catch (PartInitException e) {
+        }
+    }
+    
     public void setProofs(List<ResoluteResult> proofTrees) {
         Object[] expandedElements = treeViewer.getExpandedElements();
         TreePath[] expandedTreePaths = treeViewer.getExpandedTreePaths();
