037b25ca4480e811db94473f42542dc95e85348b
Andrew Gacek <andrew.gacek@gmail.com>
2014-04-08 13:45:08 -0500

    Output sexp to workspace

--- fm-workbench/aadl2sexp/src/com/rockwellcollins/atc/aadl2sexp/handlers/Aadl2SexpHandler.java
+++ fm-workbench/aadl2sexp/src/com/rockwellcollins/atc/aadl2sexp/handlers/Aadl2SexpHandler.java
@@ -1,8 +1,15 @@
 package com.rockwellcollins.atc.aadl2sexp.handlers;
 
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.xtext.util.StringInputStream;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.SystemImplementation;
 import org.osate.aadl2.instance.SystemInstance;
@@ -26,17 +33,25 @@ public class Aadl2SexpHandler extends AadlHandler {
                 try {
                     si = InstantiateModel.buildInstanceModelFile(sysimpl);
                 } catch (Exception e) {
-                    Dialog.showError("Model Instantiate", "Error while re-instantiating the model: "
-                            + e.getMessage());
+                    Dialog.showError("Model Instantiate",
+                            "Error while re-instantiating the model: " + e.getMessage());
                     return Status.CANCEL_STATUS;
                 }
             }
-            
-            System.out.println(InstanceToSexp.convert(si));
+
+            Sexp sexp = InstanceToSexp.convert(si);
+            Path originalPath = new Path(root.eResource().getURI().toPlatformString(true));
+            IPath path = originalPath.removeLastSegments(1).append(si.getName() + ".sexp");
+            IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+            file.delete(true, monitor);
+            try (InputStream in = new StringInputStream(sexp.toString())) {
+                file.create(new StringInputStream(sexp.toString()), true, monitor);
+            }
+
         } catch (Throwable e) {
             return new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0, e.getMessage(), e);
         }
-        
+
         return Status.OK_STATUS;
     }
 
