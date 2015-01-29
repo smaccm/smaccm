9bc6a318c79e5a7e4271796fd8eb7f1fe68d1844
Andrew Gacek <andrew.gacek@gmail.com>
2015-01-29 16:45:15 -0600

    Fix name shadowing bug for namedelements

--- fm-workbench/resolute/com.rockwellcollins.atc.resolute.analysis/src/com/rockwellcollins/atc/resolute/analysis/execution/NamedElementComparator.java
+++ fm-workbench/resolute/com.rockwellcollins.atc.resolute.analysis/src/com/rockwellcollins/atc/resolute/analysis/execution/NamedElementComparator.java
@@ -7,6 +7,19 @@ import org.osate.aadl2.NamedElement;
 public class NamedElementComparator implements Comparator<NamedElement> {
     @Override
     public int compare(NamedElement arg0, NamedElement arg1) {
-        return arg0.getName().compareToIgnoreCase(arg1.getName());
+        if (arg0 == arg1) {
+            return 0;
+        }
+        
+        int r = arg0.getName().compareToIgnoreCase(arg1.getName());
+        if (r != 0) {
+            return r;
+        }
+        
+        r = Integer.compare(arg0.hashCode(), arg1.hashCode());
+        if (r == 0) {
+            throw new IllegalArgumentException("Unable to order distinct objects via hashcode");
+        }
+        return r;
     }
 }
