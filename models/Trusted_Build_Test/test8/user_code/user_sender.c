30e2e9e1947edd6be1755f769eecd20b6e6a32ae
Michael Whalen <mike.whalen@gmail.com>
2015-02-02 04:50:58 -0600

    MWW: a number of small fixes to examples; I should really get rid of the camkes assembly files, too; these are transient.

--- models/Trusted_Build_Test/test8/user_code/user_sender.c
+++ models/Trusted_Build_Test/test8/user_code/user_sender.c
@@ -11,10 +11,10 @@ void periodic_ping(uint32_t periodic_100_ms) {
    
    printf("sender: sending test data: (%f, %f) to receiver \n", test_data.field1, test_data.field2);
    
-   bool result = ping_Output1(test_data);
+   bool result = ping_Output1(&test_data);
    printf("first attempt at pinging receiver was: %d. \n", result); 
 
-   result = ping_Output1(test_data);
+   result = ping_Output1(&test_data);
    printf("second attempt at pinging receiver was: %d. \n", result); 
 
 }
