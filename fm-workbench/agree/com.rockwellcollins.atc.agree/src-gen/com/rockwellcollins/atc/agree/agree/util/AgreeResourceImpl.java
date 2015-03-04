39c00f4a1b5df77eb82dc330f76db98f9003221c
unknown <jdbackes@CRM12762.rockwellcollins.com>
2015-03-04 09:32:13 -0600

    adding some new grammar files

---
+++ fm-workbench/agree/com.rockwellcollins.atc.agree/src-gen/com/rockwellcollins/atc/agree/agree/util/AgreeResourceImpl.java
@@ -0,0 +1,30 @@
+/**
+ */
+package com.rockwellcollins.atc.agree.agree.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see com.rockwellcollins.atc.agree.agree.util.AgreeResourceFactoryImpl
+ * @generated
+ */
+public class AgreeResourceImpl extends XMLResourceImpl
+{
+  /**
+   * Creates an instance of the resource.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param uri the URI of the new resource.
+   * @generated
+   */
+  public AgreeResourceImpl(URI uri)
+  {
+    super(uri);
+  }
+
+} //AgreeResourceImpl
