b1c64532a9f18e104e735fe0a220d835be549e90
unknown <jdbackes@CRM12762.rockwellcollins.com>
2015-03-18 13:47:54 -0500

    adding some grammar files

---
+++ fm-workbench/agree/com.rockwellcollins.atc.agree/src-gen/com/rockwellcollins/atc/agree/agree/LatchedStatement.java
@@ -0,0 +1,18 @@
+/**
+ */
+package com.rockwellcollins.atc.agree.agree;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Latched Statement</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see com.rockwellcollins.atc.agree.agree.AgreePackage#getLatchedStatement()
+ * @model
+ * @generated
+ */
+public interface LatchedStatement extends SynchStatement
+{
+} // LatchedStatement
