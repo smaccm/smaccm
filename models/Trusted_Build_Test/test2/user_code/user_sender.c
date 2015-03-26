486e3d34414ca454b7289137b5bd09fd3556de65
Michael Whalen <mike.whalen@gmail.com>
2015-03-25 20:11:52 -0500

    MWW: updated for new parameter passing conventions.

--- models/Trusted_Build_Test/test2/user_code/user_sender.c
+++ models/Trusted_Build_Test/test2/user_code/user_sender.c
@@ -2,9 +2,9 @@
 #include <sender.h>
 
 
-void periodic_ping(const uint32_t periodic_100_ms) {
+void periodic_ping(const uint64_t *periodic_100_ms) {
 
-   printf("sender ping received (%d).  Writing to receiver \n", periodic_100_ms);
+   printf("sender ping received (%lu).  Writing to receiver \n", *periodic_100_ms);
    
    test2__a_struct_impl test_data;
    test_data.field1 = 1.0;
