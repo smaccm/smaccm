c90212c59a08029f26ce0c986fcb553d1a6697ad
Daniel DaCosta <chaosape@chaosape.com>
2015-03-27 18:56:56 -0500

    Modified test8 but a build failure remains.

--- models/Trusted_Build_Test/test8/user_code/user_receiver.c
+++ models/Trusted_Build_Test/test8/user_code/user_receiver.c
@@ -1,10 +1,11 @@
 #include <smaccm_receiver.h>
 #include <stdio.h>
+#include <inttypes.h>
 
-void periodic_ping(/* const */uint32_t periodic_1000_ms) {
+void periodic_ping(const uint64_t * periodic_1000_ms) {
 	test8__a_struct_impl test_data;
 	
-	printf("receiver: periodic dispatch received at time: %d", periodic_1000_ms); 
+	printf("receiver: periodic dispatch received at time: %" PRIu64 "\n", *periodic_1000_ms); 
     printf("receiver: checking value of test_data on Input1 \n");
     ping_Input1(&test_data);
     printf("receiver: test_data (%f, %f)\n", test_data.field1, test_data.field2);
