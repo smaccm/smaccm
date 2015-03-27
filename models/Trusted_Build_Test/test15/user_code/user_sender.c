aaa6db12274f14945c4e2498d928311377969e0d
Daniel DaCosta <chaosape@chaosape.com>
2015-03-27 17:06:57 -0500

    Interface adjusted. This test suffers from what I think is a translation problem.

--- models/Trusted_Build_Test/test15/user_code/user_sender.c
+++ models/Trusted_Build_Test/test15/user_code/user_sender.c
@@ -1,9 +1,9 @@
 #include <smaccm_sender.h>
 #include <sender.h>
+#include <inttypes.h>
 
+void periodic_ping(const uint64_t * periodic_100_ms) {
 
-void periodic_ping(const uint32_t periodic_100_ms) {
-
-   printf("sender ping received (%d).  Writing to receiver \n", periodic_100_ms);
+   printf("sender ping received (%" PRIu64 ").  Writing to receiver \n", *periodic_100_ms);
    ping_Output1(); 
 }
