ba34c3ff4f5255878d39a7a3ef69e1a30b5d18ad
Daniel DaCosta <chaosape@chaosape.com>
2015-03-27 14:34:36 -0500

    Adjusted interfaces. Tested on qemu. Removed generated file.

--- models/Trusted_Build_Test/test1/user_code/user_receiver1.c
+++ models/Trusted_Build_Test/test1/user_code/user_receiver1.c
@@ -2,7 +2,6 @@
 #include <receiver.h>
 
 
-void ping_received(uint32_t *periodic_100_ms) {
-
+void ping_received(const uint32_t *periodic_100_ms) {
    printf("receiver1 ping received (%d)\n", *periodic_100_ms);
 }
--- models/Trusted_Build_Test/test1/user_code/user_sender.c
+++ models/Trusted_Build_Test/test1/user_code/user_sender.c
@@ -1,10 +1,11 @@
 #include <smaccm_sender.h>
 #include <sender.h>
+#include <inttypes.h>
 
 
-void periodic_ping(uint64_t *periodic_100_ms) {
+void periodic_ping(const uint64_t *periodic_100_ms) {
 
-   printf("sender ping received (%llu).  Writing to receiver \n", *periodic_100_ms);
+   printf("sender ping received (%" PRIu64 ").  Writing to receiver \n", *periodic_100_ms);
    uint32_t to_send = ((uint32_t)*periodic_100_ms) + 1;
    ping_Output1(&to_send);
 	
