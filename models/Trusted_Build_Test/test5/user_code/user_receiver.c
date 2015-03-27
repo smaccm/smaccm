0fc4bb5ff0b926623d8269ea3567bb925508a1b8
Daniel DaCosta <chaosape@chaosape.com>
2015-03-27 18:10:15 -0500

    test 4 modified, however there remains a build failure.

--- models/Trusted_Build_Test/test5/user_code/user_receiver.c
+++ models/Trusted_Build_Test/test5/user_code/user_receiver.c
@@ -1,10 +1,11 @@
 #include <smaccm_receiver.h>
 #include <receiver.h>
+#include <inttypes.h>
 
-void ping_received(test5__a_struct_impl *test_data) {
+void ping_received(const test5__a_struct_impl *test_data) {
    printf("receiver: ping_received invoked (%f, %f)\n", test_data->field1, test_data->field2);
 }
 
-void periodic_ping(/* const */uint32_t periodic_1000_ms) {
-	printf("receiver: periodic dispatch received at time: %d \n", periodic_1000_ms); 
+void periodic_ping(const uint32_t * periodic_1000_ms) {
+	printf("receiver: periodic dispatch received at time: %" PRIu64 " \n", *periodic_1000_ms); 
 }
--- models/Trusted_Build_Test/test5/user_code/user_sender.c
+++ models/Trusted_Build_Test/test5/user_code/user_sender.c
@@ -1,10 +1,10 @@
 #include <smaccm_sender.h>
 #include <sender.h>
+#include <inttypes.h>
 
+void periodic_ping(const uint64_t * periodic_100_ms) {
 
-void periodic_ping(uint32_t periodic_100_ms) {
-
-   printf("sender: periodic dispatch received (%d).  Writing to receiver \n", periodic_100_ms);
+   printf("sender: periodic dispatch received (%" PRIu64 ").  Writing to receiver \n", *periodic_100_ms);
    
    test5__a_struct_impl test_data;
    test_data.field1 = 1.0;
