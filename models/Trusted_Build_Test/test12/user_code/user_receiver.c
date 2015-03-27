6e2ca2ff3694e9b888f380aada34b5f02efc12f4
Daniel DaCosta <chaosape@chaosape.com>
2015-03-27 16:10:07 -0500

    Modified interface face. Could not test due to aadl4rtos failure.

--- models/Trusted_Build_Test/test12/user_code/user_receiver.c
+++ models/Trusted_Build_Test/test12/user_code/user_receiver.c
@@ -2,8 +2,8 @@
 #include <receiver.h>
 
 
-void periodic_ping(/* const */uint32_t periodic_1000_ms) {
-	printf("receiver: periodic dispatch received at time: %d \n", periodic_1000_ms); 
+void periodic_ping( const uint32_t * periodic_1000_ms) {
+	printf("receiver: periodic dispatch received at time: %" PRIu64 " \n", *periodic_1000_ms); 
 	
 	test10__a_struct_impl test_data;
 	bool result = true; 
