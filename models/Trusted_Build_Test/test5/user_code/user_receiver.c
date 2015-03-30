581773c3ff325391180485611e50b03ba6237aec
Daniel DaCosta <chaosape@chaosape.com>
2015-03-30 11:59:27 -0500

    Tested 2-9.

--- models/Trusted_Build_Test/test5/user_code/user_receiver.c
+++ models/Trusted_Build_Test/test5/user_code/user_receiver.c
@@ -6,12 +6,6 @@ void ping_received(const test5__a_struct_impl *test_data) {
    printf("receiver: ping_received invoked (%f, %f)\n", test_data->field1, test_data->field2);
 }
 
-<<<<<<< HEAD
-void periodic_ping(const uint32_t * periodic_1000_ms) {
-	printf("receiver: periodic dispatch received at time: %" PRIu64 " \n", *periodic_1000_ms); 
-}
-=======
 void periodic_ping(const uint64_t * periodic_1000_ms) {
 	printf("receiver: periodic dispatch received at time: %" PRIu64 " \n", *periodic_1000_ms); 
 }
->>>>>>> upstream/develop
