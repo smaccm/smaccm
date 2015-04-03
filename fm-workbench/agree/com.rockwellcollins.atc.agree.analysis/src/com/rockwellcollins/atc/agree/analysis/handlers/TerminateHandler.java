78e5d03e90d43ba4db20f0bfe8d63f69c60b6ddb
Andrew Gacek <andrew.gacek@gmail.com>
2015-04-03 11:10:50 -0500

    Use current monitor reference to cancel one job at a time

--- fm-workbench/agree/com.rockwellcollins.atc.agree.analysis/src/com/rockwellcollins/atc/agree/analysis/handlers/TerminateHandler.java
+++ fm-workbench/agree/com.rockwellcollins.atc.agree.analysis/src/com/rockwellcollins/atc/agree/analysis/handlers/TerminateHandler.java
@@ -1,28 +1,23 @@
 package com.rockwellcollins.atc.agree.analysis.handlers;
 
-import java.util.Queue;
-
-import jkind.api.results.JKindResult;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public class TerminateHandler extends AbstractHandler {
-    private final IProgressMonitor monitor;
-    private final Queue<JKindResult> queue;
+    private final AtomicReference<IProgressMonitor> monitorRef;
 
-    public TerminateHandler(IProgressMonitor monitor, Queue<JKindResult> queue) {
-        this.monitor = monitor;
-        this.queue = queue;
+    public TerminateHandler(AtomicReference<IProgressMonitor> monitorRef) {
+        this.monitorRef = monitorRef;
     }
 
     @Override
     public Object execute(ExecutionEvent event) {
-//        monitor.setCanceled(true);
-        JKindResult result = queue.peek();
-        if(result != null){
-            result.cancel();
+        IProgressMonitor monitor = monitorRef.get();
+        if (monitor != null) {
+            monitor.setCanceled(true);
         }
         return null;
     }
