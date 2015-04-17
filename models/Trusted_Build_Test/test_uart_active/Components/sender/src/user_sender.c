b058920c1017b683ea6fc4a7a47a75c73f47c00e
John Backes <john.backes@rockwellcollins.com>
2015-04-17 15:34:40 -0500

    commiting build stuff

---
+++ models/Trusted_Build_Test/test_uart_active/Components/sender/src/user_sender.c
@@ -0,0 +1,13 @@
+#include <smaccm_sender.h>
+#include <sender.h>
+
+
+void periodic_ping(const uint64_t *periodic_100_ms) {
+
+   test_uart__uart_packet_impl packet;
+   printf("sender ping received (%llu).  Writing to receiver \n", *periodic_100_ms);
+   packet.uart_num = 0;
+   packet.datum = (uint32_t)(*periodic_100_ms) + 1;
+   ping_Output1(&packet); 
+	
+}
