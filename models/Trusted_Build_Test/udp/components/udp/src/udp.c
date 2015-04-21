6b83ad8ef8f6bac455d9a3843bbf19b1af85fc4a
Andrew Gacek <andrew.gacek@gmail.com>
2015-04-21 14:27:03 -0500

    New approach to UDP example

--- models/Trusted_Build_Test/udp/components/udp/src/udp.c
+++ models/Trusted_Build_Test/udp/components/udp/src/udp.c
@@ -1,2 +1,47 @@
 #include <udp.h>
 
+void putchar_putchar(int c) {
+    printf("Putchar: %c\n", c);
+}
+
+bool client_input_write_udp__packet_i(const udp__packet_i * arg) {
+    // Take packet from AADL interface and pass it to UDPServer
+    int len = udp_send_send(arg->buffer, arg->len, arg->addr);
+    return (len == arg->len);
+}
+
+int run(void) {
+    while (1) {
+	// TODO: Read UDP packets from UDPServer and pass them along on AADL interface
+	// TODO: Right now we do this via callback but that does not rate limit at all
+	sleep(1000);
+    }
+
+    return 0;
+}
+
+void udp_has_data(void *cookie) {
+    int status = 0;
+    while (status == 0) {
+        unsigned int len;
+        uint16_t port;
+        ip_addr_t addr;
+        status = echo_recv_poll(&len, &port, &addr);
+        if (status != -1) {
+	    udp__packet_i packet;
+	    // TODO: Should we be doing this check or just rely on the underlying driver?
+	    if (len > 4096) {
+		len = 4096;
+	    }
+	    memcpy(packet.buffer, udp_recv_buf, len);
+	    packet.len = len;
+	    packet.addr = addr;
+	    udp_client_output_write_udp__packet_i(&packet);
+        }
+    }
+    echo_recv_ready_reg_callback(echo_has_data, cookie);
+}
+
+void pre_init() {
+    udp_recv_ready_reg_callback(udp_has_data, NULL);
+}
