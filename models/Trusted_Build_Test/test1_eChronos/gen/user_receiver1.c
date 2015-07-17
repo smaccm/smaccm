c655eed3cb1ef9c146abf22bbe2fddd7e19ec840
Michael Whalen <mike.whalen@gmail.com>
2015-07-16 20:05:23 -0500

    MWW: added for Robert

--- models/Trusted_Build_Test/test1_eChronos/gen/user_receiver1.c
+++ models/Trusted_Build_Test/test1_eChronos/gen/user_receiver1.c
@@ -2,8 +2,9 @@
 #include <debug.h>
 
 void ping_received1(const uint32_t *periodic_100_ms) {
-
    debug_print("receiver1 ping received");
    debug_printhex32(*periodic_100_ms);
    debug_println("");
+   uint32_t newValue = 2*(*periodic_100_ms);
+   ping_Output2(&newValue);
 }
