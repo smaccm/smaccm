d9ae6752698987a36725d267dc1f8431691add31
julien <julien@gunnm.org>
2015-10-31 13:06:37 -0400

    * Start adding support for Dcase export

---
+++ fm-workbench/resolute/com.rockwellcollins.atc.resolute.analysis/src/com/rockwellcollins/atc/resolute/analysis/export/Dcase.java
@@ -0,0 +1,19 @@
+package com.rockwellcollins.atc.resolute.analysis.export;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osate.aadl2.util.OsateDebug;
+
+import com.rockwellcollins.atc.resolute.analysis.results.ClaimResult;
+import com.rockwellcollins.atc.resolute.analysis.results.ResoluteResult;
+
+public class Dcase {
+
+    private static int nodeCount;
+
+    public static void claimToGSN(ClaimResult claim) {
+    	OsateDebug.osateDebug("GSN export" ,  "Export to GSN");
+    }
+
+}
