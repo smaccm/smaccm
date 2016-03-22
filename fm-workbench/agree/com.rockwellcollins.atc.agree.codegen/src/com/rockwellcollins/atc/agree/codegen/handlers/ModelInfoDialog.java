a3bff926e3c0c8426bce86c3cbe85acb72bc2a09
Andrew Gacek <andrew.gacek@gmail.com>
2016-03-22 10:09:55 -0500

    Add validation to model info dialog

--- fm-workbench/agree/com.rockwellcollins.atc.agree.codegen/src/com/rockwellcollins/atc/agree/codegen/handlers/ModelInfoDialog.java
+++ fm-workbench/agree/com.rockwellcollins.atc.agree.codegen/src/com/rockwellcollins/atc/agree/codegen/handlers/ModelInfoDialog.java
@@ -1,7 +1,14 @@
 package com.rockwellcollins.atc.agree.codegen.handlers;
 
-import org.eclipse.jface.dialogs.Dialog;
+import java.io.File;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -15,7 +22,7 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-public class ModelInfoDialog extends Dialog {
+public class ModelInfoDialog extends TitleAreaDialog {
 	private final class DirChooserListner implements Listener {
 		public void handleEvent(Event event) {
 			// Get saved or entered output directory
@@ -58,8 +65,13 @@ public class ModelInfoDialog extends Dialog {
 	private ModelInfo updatedInfo;
 
 	private Text outputText;
+
 	private Text originalText;
+	private ControlDecoration originalTextError;
+
 	private Text updatedText;
+	private ControlDecoration updatedTextError;
+
 	private Text subsystemText;
 
 	protected ModelInfoDialog(Shell parentShell, ModelInfo savedInfo) {
@@ -73,19 +85,32 @@ public class ModelInfoDialog extends Dialog {
 	}
 
 	@Override
-	protected boolean isResizable() {
-		return true;
+	public void create() {
+		super.create();
+		setTitle("Model Info for Inserting Simulink Observer");
+		// setMessage("This is a TitleAreaDialog", IMessageProvider.INFORMATION);
 	}
 
 	@Override
-	protected void configureShell(Shell shell) {
-		super.configureShell(shell);
-		shell.setText("Model Info for Inserting Simulink Observer");
+	protected boolean isResizable() {
+		return true;
+
+	}
+
+	@Override
+	public boolean isHelpAvailable() {
+		return false;
 	}
 
 	@Override
 	protected Control createDialogArea(Composite parent) {
-		Composite container = (Composite) super.createDialogArea(parent);
+		Composite area = (Composite) super.createDialogArea(parent);
+		Composite container = new Composite(area, SWT.NONE);
+
+		FieldDecoration fieldDecoration = FieldDecorationRegistry.getDefault().getFieldDecoration(
+				FieldDecorationRegistry.DEC_ERROR);
+		Image errorImage = fieldDecoration.getImage();
+
 		GridLayout layout = new GridLayout(3, false);
 		layout.marginRight = 5;
 		layout.marginLeft = 10;
@@ -101,7 +126,9 @@ public class ModelInfoDialog extends Dialog {
 		outputText.setText(savedInfo.outputDirPath);
 		gridData = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		gridData.widthHint = 300;
+		gridData.horizontalIndent = 10;
 		outputText.setLayoutData(gridData);
+		outputText.addModifyListener(e -> validate());
 
 		Button browseOutputButton = new Button(container, SWT.PUSH);
 		browseOutputButton.setText("Browse");
@@ -120,7 +147,13 @@ public class ModelInfoDialog extends Dialog {
 		originalText.setText(savedInfo.originalModelName);
 		gridData = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		gridData.widthHint = 300;
+		gridData.horizontalIndent = 10;
 		originalText.setLayoutData(gridData);
+		originalText.addModifyListener(e -> validate());
+
+		originalTextError = new ControlDecoration(originalText, SWT.TOP | SWT.LEFT);
+		originalTextError.setImage(errorImage);
+		originalTextError.hide();
 
 		Button browseOriginalButton = new Button(container, SWT.PUSH);
 		browseOriginalButton.setText("Browse");
@@ -139,7 +172,13 @@ public class ModelInfoDialog extends Dialog {
 		updatedText.setText(savedInfo.updatedModelName);
 		gridData = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		gridData.horizontalSpan = 2;
+		gridData.horizontalIndent = 10;
 		updatedText.setLayoutData(gridData);
+		updatedText.addModifyListener(e -> validate());
+
+		updatedTextError = new ControlDecoration(updatedText, SWT.TOP | SWT.LEFT);
+		updatedTextError.setImage(errorImage);
+		updatedTextError.hide();
 
 		Label subsystemLabel = new Label(container, SWT.NONE);
 		subsystemLabel.setText("Subsystem Name:");
@@ -151,9 +190,58 @@ public class ModelInfoDialog extends Dialog {
 		subsystemText.setText(savedInfo.subsystemName);
 		gridData = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		gridData.horizontalSpan = 2;
+		gridData.horizontalIndent = 10;
 		subsystemText.setLayoutData(gridData);
+		subsystemText.addModifyListener(e -> validate());
 
 		return container;
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		Control result = super.createContents(parent);
+		validate();
+		return result;
+	}
+
+	protected void validate() {
+		boolean hasErrors = false;
+
+		if (!updatedText.getText().endsWith(".slx")) {
+			setErrorMessage("Updated model name must have .slx extension");
+			setOkEnabled(false);
+			updatedTextError.show();
+			hasErrors = true;
+		} else {
+			updatedTextError.hide();
+		}
+
+		if (!new File(originalText.getText()).exists()) {
+			originalTextError.setDescriptionText("Original model file does not exist");
+			setErrorMessage("Original model file does not exist");
+			setOkEnabled(false);
+			originalTextError.show();
+			hasErrors = true;
+		} else if (!originalText.getText().endsWith(".slx")) {
+			setErrorMessage("Original model must have .xls extension");
+			setOkEnabled(false);
+			originalTextError.show();
+			hasErrors = true;
+		} else {
+			originalTextError.hide();
+		}
+
+		if (!hasErrors) {
+			setErrorMessage(null);
+			setOkEnabled(true);
+		}
+	}
+
+	private void setOkEnabled(boolean enabled) {
+		Button ok = getButton(IDialogConstants.OK_ID);
+		if (ok != null) {
+			ok.setEnabled(enabled);
+		}
 	}
 
 	@Override
