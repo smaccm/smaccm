19aee1b255ba6c2652bf7ffc6cd75efa1046492f
Janet Liu <jliu29@CRP21563.rockwellcollins.com>
2016-05-11 14:21:20 -0500

    1. Get rid of invoking MATLAB. 2. Get rid of verify button.3. Not closing system before incserting observer. 4. Updated update button enable logic. 5. Updated GUI label and button names to clarify usage. 6. Allow '/' in path as Simulink subsystem separator.

--- fm-workbench/agree/com.rockwellcollins.atc.agree.codegen/src/com/rockwellcollins/atc/agree/codegen/handlers/MATLABFunctionHandler.java
+++ fm-workbench/agree/com.rockwellcollins.atc.agree.codegen/src/com/rockwellcollins/atc/agree/codegen/handlers/MATLABFunctionHandler.java
@@ -147,20 +147,6 @@ public class MATLABFunctionHandler extends ModifyingAadlHandler {
 					
 					writeToFile(modelUpdateScriptPath,
 							modelUpdateScript.toString());
-
-					// create batch file into the output folder
-					MATLABInvocationScriptCreator matlabInvokeScript = new MATLABInvocationScriptCreator(
-							dirStr, modelUpdateScriptName);
-					
-					String batchFileName = matlabFunction.name
-							+ "_cmd_line.bat";
-
-					Path batchPath = Paths.get(dirStr, batchFileName);
-					String batchPathStr = dirStr + "\\" + batchFileName;
-					writeToFile(batchPath, matlabInvokeScript.toString());
-
-					// invoke the batch file
-					Runtime.getRuntime().exec(batchPathStr);
 				}
 			}
 			return Status.OK_STATUS;
@@ -267,7 +253,7 @@ public class MATLABFunctionHandler extends ModifyingAadlHandler {
 	}
 	
 	private String readPathFromAADLProperty(String inputStr) {
-		String updatedStr = inputStr.replaceAll("(\\\\+|/+)", Matcher.quoteReplacement(System.getProperty("file.separator")));
+		String updatedStr = inputStr.replaceAll("(\\\\+)", Matcher.quoteReplacement(System.getProperty("file.separator")));
 		return updatedStr;
 	}
 
