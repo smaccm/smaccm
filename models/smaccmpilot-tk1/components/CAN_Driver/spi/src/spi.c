b701943f663836a5a5e118e375a1747fd2fa377e
Andrew Gacek <andrew.gacek@gmail.com>
2017-01-16 11:04:15 -0600

    Use new chip_select interface for spi

--- models/smaccmpilot-tk1/components/CAN_Driver/spi/src/spi.c
+++ models/smaccmpilot-tk1/components/CAN_Driver/spi/src/spi.c
@@ -86,7 +86,7 @@ get_slave(int id)
     return NULL;
 }
 
-static inline void
+static void
 chip_select(spi_bus_t* slave, int state)
 {
     gpio_spi_can1_cs(state);
@@ -127,7 +127,7 @@ spi__init(void)
     int err;
 
     /* Initialise the SPI bus */
-    err = tegra_spi_init(SPI_PORT, spi1_reg, NULL, &clock_sys, &spi_bus);
+    err = tegra_spi_init(SPI_PORT, spi1_reg, chip_select, NULL, &clock_sys, &spi_bus);
     assert(!err);
     if (err) {
         ZF_LOGE("Failed to initialise SPI port");
@@ -152,9 +152,6 @@ do_spi_transfer(const struct spi_slave* slave, void* txbuf, unsigned int wcount,
         spi_prepare_transfer(spi_bus, &slave->cfg);
         cur_slave_id = slave->id;
     }
-    ZF_LOGD("About to call chip_select()");
-    chip_select(spi_bus, SPI_CS_ASSERT);
-    ZF_LOGD("Chip select done");
     /* Begin the transfer */
     spi_mutex_lock();
     ret = spi_xfer(spi_bus, txbuf, wcount, rxbuf, rcount, spi_complete_callback, &status);
@@ -164,8 +161,6 @@ do_spi_transfer(const struct spi_slave* slave, void* txbuf, unsigned int wcount,
         bus_sem_wait();
         ret = status;
     }
-    ZF_LOGD("Releasing chip select");
-    chip_select(spi_bus, SPI_CS_RELEASE);
     return ret;
 }
 
