5d8bf1c05ab22e91dd4ca5ad47fb9721177d2677
Philip Alldredge <pwa0001@uah.edu>
2018-03-23 08:37:48 -0500

    Allow executing AGREE analysis and TCG from graphical editor.
    Updates handlers for AGREE and TCG to enable top-level AGREE menu items
    when working in the graphical editor.

--- fm-workbench/agree/com.rockwellcollins.atc.agree.analysis/src/com/rockwellcollins/atc/agree/analysis/handlers/ModifyingAadlHandler.java
+++ fm-workbench/agree/com.rockwellcollins.atc.agree.analysis/src/com/rockwellcollins/atc/agree/analysis/handlers/ModifyingAadlHandler.java
@@ -1,10 +1,17 @@
 package com.rockwellcollins.atc.agree.analysis.handlers;
 
+import java.io.IOException;
+
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.resource.SaveOptions;
 import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.osate.aadl2.modelsupport.resources.OsateResourceUtil;
 
 public abstract class ModifyingAadlHandler extends AadlHandler {
 	@Override
@@ -15,7 +22,25 @@ public abstract class ModifyingAadlHandler extends AadlHandler {
 				// modification may update text editors so we need to run in the SWT thread
 				IStatus[] status = new IStatus[1];
 				getWindow().getShell().getDisplay().syncExec(() -> {
-					status[0] = xtextEditor.getDocument().modify(getUnitOfWork(uri, monitor));
+					if (xtextEditor == null) {
+						final ResourceSet rs = OsateResourceUtil.getResourceSet();
+						status[0] = getWorker(uri, monitor).apply(rs);
+
+						// Save the model
+						final EObject eobj = rs.getEObject(uri, true);
+						if (eobj != null) {
+							final Resource resource = eobj.eResource();
+							if (resource != null) {
+								try {
+									resource.save(SaveOptions.newBuilder().format().getOptions().toOptionsMap());
+								} catch (final IOException e) {
+									throw new RuntimeException(e);
+								}
+							}
+						}
+					} else {
+						status[0] = xtextEditor.getDocument().modify(getUnitOfWork(uri, monitor));
+					}
 				});
 				return status[0];
 			};
