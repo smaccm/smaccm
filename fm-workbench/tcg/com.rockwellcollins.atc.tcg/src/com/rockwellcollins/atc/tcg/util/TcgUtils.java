34877f69bb22c85380b30ddd02b058631660c332
Karl Hoech <karl.hoech@rockwellcollins.com>
2018-09-27 10:03:42 -0500

    Replace use of deprecated EMFIndexRetrieval in TCG

--- fm-workbench/tcg/com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/util/TcgUtils.java
+++ fm-workbench/tcg/com.rockwellcollins.atc.tcg/src/com/rockwellcollins/atc/tcg/util/TcgUtils.java
@@ -1,21 +1,21 @@
 /*
-Copyright (c) 2016, Rockwell Collins.
+Copyright (c) 2018, Rockwell Collins.
 Developed with the sponsorship of Defense Advanced Research Projects Agency (DARPA).
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this data, 
-including any software or models in source or binary form, as well as any drawings, specifications, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this data,
+including any software or models in source or binary form, as well as any drawings, specifications,
 and documentation (collectively "the Data"), to deal in the Data without restriction, including
-without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Data, and to permit persons to whom the Data is furnished to do so, 
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Data, and to permit persons to whom the Data is furnished to do so,
 subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or 
+The above copyright notice and this permission notice shall be included in all copies or
 substantial portions of the Data.
 
-THE DATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-IN NO EVENT SHALL THE AUTHORS, SPONSORS, DEVELOPERS, CONTRIBUTORS, OR COPYRIGHT HOLDERS BE LIABLE 
-FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+THE DATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS, SPONSORS, DEVELOPERS, CONTRIBUTORS, OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE DATA OR THE USE OR OTHER DEALINGS IN THE DATA.
 */
 
@@ -23,10 +23,14 @@ package com.rockwellcollins.atc.tcg.util;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.HashSet;
 
+import org.eclipse.core.resources.IFile;
+import org.osate.aadl2.AadlPackage;
 import org.osate.aadl2.Classifier;
 import org.osate.aadl2.ComponentImplementation;
-import org.osate.xtext.aadl2.properties.util.EMFIndexRetrieval;
+import org.osate.aadl2.ModelUnit;
+import org.osate.aadl2.modelsupport.util.AadlUtil;
 
 import com.rockwellcollins.atc.tcg.TcgException;
 
@@ -46,12 +50,18 @@ public class TcgUtils {
 	}
 
 	public static ComponentImplementation getComponentImplFromString(String qualifiedName) {
-		Classifier cl = EMFIndexRetrieval.getClassifierInWorkspace(qualifiedName);
-		if (cl instanceof ComponentImplementation) {
-			return (ComponentImplementation)cl;
-		} else {
-			throw new TcgException("Error: test suite implementation under test: [" + qualifiedName + "] does not correspond to any " + 
-		     "system implementation in the workspace.\n");
+		HashSet<IFile> files = org.osate.aadl2.modelsupport.modeltraversal.TraverseWorkspace.getAadlFilesInWorkspace();
+		for (IFile file : files) {
+			ModelUnit target = (ModelUnit) AadlUtil.getElement(file);
+			if (target instanceof AadlPackage) {
+				Classifier cl = org.osate.aadl2.modelsupport.scoping.Aadl2GlobalScopeUtil.get(target,
+						org.osate.aadl2.Aadl2Package.eINSTANCE.getComponentImplementation(), qualifiedName);
+				if (cl instanceof ComponentImplementation) {
+					return (ComponentImplementation) cl;
+				}
+			}
 		}
+		throw new TcgException("Error: test suite implementation under test: [" + qualifiedName
+				+ "] does not correspond to any " + "system implementation in the workspace.\n");
 	}
 }
